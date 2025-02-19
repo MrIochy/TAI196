bc072d00069aadc49c4816b024b5668e9b80d541
MrIochy <157193100+MrIochy@users.noreply.github.com>
2025-02-19 17:51:48 -0600

    Endpoint: Eliminar una tarea

--- Repaso_FASTAPI/entornoTareas/main.py
+++ Repaso_FASTAPI/entornoTareas/main.py
@@ -56,3 +56,12 @@ def actualizar_tarea(id: int, tarea_actualizada: dict):
             tareas[index].update(tarea_actualizada)
             return tareas[index]
     raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+# Endpoint: Eliminar una tarea
+@app.delete('/tarea/{id}', tags=['Operaciones CRUD'])
+def Eliminar(id: int):
+    for index, tarea in enumerate(tareas):
+        if tarea["id"] == id:
+            del tareas[index]
+            return {"mensaje": "Usuario eliminado correctamente"}
+    raise HTTPException(status_code=404, detail="Usuario no encontrado")
